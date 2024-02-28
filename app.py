3d02e2c81039d92bdc2cc37813e0d90ea0c1a321
myamasaki1019 <135980618+myamasaki1019@users.noreply.github.com>
2024-02-28 18:02:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,8 @@ from backend.utils import format_as_ndjson, format_stream_response, generateFilt
 bp = Blueprint("routes", __name__, static_folder="static", template_folder="static")
 
 # UI configuration (optional)
-UI_TITLE = os.environ.get("UI_TITLE") or "Contoso"
+# UI_TITLE = os.environ.get("UI_TITLE") or "Contoso"
+UI_TITLE = "作業効率化WG"
 UI_LOGO = os.environ.get("UI_LOGO")
 UI_CHAT_LOGO = os.environ.get("UI_CHAT_LOGO")
 UI_CHAT_TITLE = os.environ.get("UI_CHAT_TITLE") or "Start chatting"
